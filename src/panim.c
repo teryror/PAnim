e00bd66e63af3eef6c93ca9f2115574a6d43c87d
Tristan Dannenberg <dtristan@hotmail.de>
2018-04-20 21:04:12 +0200

    Factor playback and encoding into separate code paths

--- src/panim.c
+++ src/panim.c
@@ -4,6 +4,7 @@ Notice: No warranty is offered or implied; use this code at your own risk.
 *******************************************************************************/
 
 #include "stdio.h"
+#include "stdint.h"
 
 #include "SDL/SDL.h"
 #include "SDL/SDL_ttf.h"
@@ -16,58 +17,96 @@ Notice: No warranty is offered or implied; use this code at your own risk.
 #define VidWidth 1280
 #define VidHeight 720
 
-static void frame_convert_test(struct SwsContext *context,
-                               AVFrame *src, AVFrame *dst)
-{
-    sws_scale(context, src->data, src->linesize, 0, src->height,
-              dst->data, dst->linesize);
-}
-
-static void frame_encode_test(AVCodecContext *context,
-                              AVFrame  *frame,
-                              AVPacket *packet,
-                              FILE *outfile)
-{
-    int ret = avcodec_send_frame(context, frame);
-    if (ret < 0) ERROR("failed to send frame for encoding!");
+typedef struct {
+    size_t length_in_frames;
+    int screen_width;
+    int screen_height;
     
-    while (ret >= 0) {
-        ret = avcodec_receive_packet(context, packet);
-        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
-            return;
-        } else if (ret < 0) ERROR("error during encoding!");
-        
-        fwrite(packet->data, 1, packet->size, outfile);
-        av_packet_unref(packet);
-    }
-}
+    SDL_Color bg_color;
+} PAnimScene;
 
-static void video_encode_test(char * filename) {
-    // 
-    // Rendering Setup
-    // 
-    
+typedef struct {
+    SDL_Window   * window;
+    SDL_Renderer * renderer;
+    SDL_Texture  * render_target;
+} PAnimEngine;
+
+static void
+panim_engine_begin_preview(PAnimEngine * pnm, PAnimScene * scene)
+{
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) ERROR("initialization failed (SDL)!");
     if (TTF_Init() != 0) ERROR("initialization failed (TTF)!");
     
-    SDL_Window *window = SDL_CreateWindow(
+    pnm->window = SDL_CreateWindow(
         "Hello, SDL!",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        VidWidth, VidHeight, 0);
-    if (window == NULL) ERROR("failed to create window!");
+        scene->screen_width,
+        scene->screen_height, 0);
+    if (pnm->window == NULL) ERROR("failed to create window!");
     
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL) ERROR("failed to create renderer!");
+    pnm->renderer = SDL_CreateRenderer(pnm->window, -1, SDL_RENDERER_ACCELERATED);
+    if (pnm->renderer == NULL) ERROR("failed to create renderer!");
     
-    SDL_Texture *text = NULL;
-    TTF_Font *font = TTF_OpenFont("Oswald-Bold.ttf", 48);
-    
-    SDL_Texture *vid_frame_texture = SDL_CreateTexture(
-        renderer, SDL_PIXELFORMAT_ARGB8888,
+    pnm->render_target = SDL_CreateTexture(
+        pnm->renderer, SDL_PIXELFORMAT_ARGB8888,
         SDL_TEXTUREACCESS_TARGET,
-        VidWidth, VidHeight);
-    if (vid_frame_texture == NULL) ERROR("failed to create render target!");
+        scene->screen_width,
+        scene->screen_height);
+    if (pnm->render_target == NULL) ERROR("failed to create render target!");
+}
+
+static void
+panim_engine_end_preview(PAnimEngine * pnm)
+{
+    SDL_DestroyRenderer(pnm->renderer);
+    SDL_DestroyWindow(pnm->window);
+    SDL_Quit();
+}
+
+static void
+panim_scene_frame_update(PAnimScene * scene, size_t t)
+{
+    unsigned char col = (unsigned char)
+        ((double)t / (double)scene->length_in_frames * 255.0);
+    scene->bg_color = (SDL_Color){ col, 0, col, 255 };
+    
+    // TODO: Check timeline for animations and update scene objects accordingly
+}
+
+static void
+panim_scene_frame_render(PAnimEngine * pnm, PAnimScene * scene)
+{
+    SDL_SetRenderTarget(pnm->renderer, pnm->render_target);
+    SDL_Color bg = scene->bg_color;
+    SDL_SetRenderDrawColor(
+        pnm->renderer, bg.r, bg.g, bg.b, bg.a);
+    
+    // TODO: Render all objects in scene
+    
+    SDL_RenderClear(pnm->renderer);
+}
+
+static void
+panim_frame_present(PAnimEngine * pnm)
+{
+    SDL_SetRenderTarget(pnm->renderer, NULL);
+    SDL_RenderClear(pnm->renderer);
+    SDL_RenderCopy(pnm->renderer, pnm->render_target, NULL, NULL);
+    
+    // TODO: Debug UI?
+    
+    SDL_RenderPresent(pnm->renderer);
+}
+
+/* 
+ * Plays back the scene in a preview window while also rendering it to a file.
+ */
+static void
+panim_scene_render(PAnimScene * scene, char * filename)
+{
+    PAnimEngine pnm = {0};
+    panim_engine_begin_preview(&pnm, scene);
     
     // 
     // Video Encoding Setup
@@ -128,34 +167,11 @@ static void video_encode_test(char * filename) {
     // Main Loop
     // 
     
-    // 60 frames for one second of footage
-    for (int i = 0; i < 240; ++i) {
-        char t = (char)((float)i / 240.0f * 255);
-        /*
-        SDL_Event e;
-        if (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) break;
-            if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_ESCAPE) break;
-        }
-        */
-        { // Render Text
-            SDL_Surface *surf = TTF_RenderText_Solid(
-                font, "Hello, World!", (SDL_Color){ t, t, t, 255 });
-            text = SDL_CreateTextureFromSurface(renderer, surf);
-            
-            SDL_FreeSurface(surf);
-        }
+    for (size_t t = 0; t < scene->length_in_frames; ++t) {
+        panim_scene_frame_update(scene, t);
+        panim_scene_frame_render(&pnm, scene);
         
-        int w, h;
-        SDL_QueryTexture(text, NULL, NULL, &w, &h);
-        SDL_Rect dst_rect = (SDL_Rect){ VidWidth/2 - w/2, VidHeight/2 - h/2, w, h }; 
-        
-        SDL_SetRenderTarget(renderer, vid_frame_texture);
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, text, NULL, &dst_rect);
-        
-        { // Encode content of the current render target
-            printf("Encoding frame %2d/240 ...\r", i);
+        { // Get backbuffer contents
             fflush(stdout);
             
             int ret = av_frame_make_writable(src_frame);
@@ -165,32 +181,50 @@ static void video_encode_test(char * filename) {
                 exit(1);
             }
             
-            SDL_RenderReadPixels(renderer, NULL, 0,
+            SDL_RenderReadPixels(pnm.renderer, NULL, 0,
                                  src_frame->data[0],
                                  src_frame->linesize[0]);
-            src_frame->pts = i;
-            
-            frame_convert_test(sws_ctx, src_frame, dst_frame);
-            frame_encode_test(cdc_ctx, dst_frame, packet, f);
+            src_frame->pts = t;
         }
         
-        SDL_SetRenderTarget(renderer, NULL);
-        SDL_RenderClear(renderer);
-        if (SDL_RenderCopy(renderer, vid_frame_texture, NULL, NULL)) {
-            printf("\nSomething happened here!\n");
+        // Convert between pixel formats (color spaces)
+        sws_scale(sws_ctx,
+                  src_frame->data, src_frame->linesize, 0, src_frame->height,
+                  dst_frame->data, dst_frame->linesize);
+        
+        { // Send frame for encoding
+            int ret = avcodec_send_frame(cdc_ctx, dst_frame);
+            if (ret < 0) ERROR("failed to send frame for encoding!");
+            
+            while (ret >= 0) {
+                ret = avcodec_receive_packet(cdc_ctx, packet);
+                if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
+                    break;
+                } else if (ret < 0) ERROR("error during encoding!");
+                
+                fwrite(packet->data, 1, packet->size, f);
+                av_packet_unref(packet);
+            }
         }
-        SDL_RenderCopy(renderer, text, NULL, &(SDL_Rect){ 0, 0, w/2, h/2 });
-        SDL_RenderPresent(renderer);
+        
+        panim_frame_present(&pnm);
     }
     
-    // 
-    // Final Touches
-    // 
-    
-    printf("DONE.                     ");
-    fflush(stdout);
-    
-    frame_encode_test(cdc_ctx, NULL, packet, f); // flush the encoder
+    { // Flush the encoder
+        fflush(stdout);
+        int ret = avcodec_send_frame(cdc_ctx, NULL);
+        if (ret < 0) ERROR("failed to send frame for encoding!");
+        
+        while (ret >= 0) {
+            ret = avcodec_receive_packet(cdc_ctx, packet);
+            if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
+                break;
+            } else if (ret < 0) ERROR("error during encoding!");
+            
+            fwrite(packet->data, 1, packet->size, f);
+            av_packet_unref(packet);
+        }
+    }
     
     uint8_t endcode[] = { 0, 0, 1, 0xb7 };
     fwrite(endcode, 1, sizeof(endcode), f);
@@ -202,21 +236,46 @@ static void video_encode_test(char * filename) {
     avcodec_free_context(&cdc_ctx);
     sws_freeContext(sws_ctx);
     
-    TTF_CloseFont(font);
-    SDL_DestroyTexture(text);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    panim_engine_end_preview(&pnm);
+}
+
+/* 
+ * Plays back the scene in a preview window without rendering to a file.
+ */
+static void
+panim_scene_play(PAnimScene * scene)
+{
+    PAnimEngine pnm = {0};
+    panim_engine_begin_preview(&pnm, scene);
+    
+    for (size_t t = 0; t < scene->length_in_frames; ++t) {
+        panim_scene_frame_update(scene, t);
+        panim_scene_frame_render(&pnm, scene);
+        panim_frame_present(&pnm);
+        
+        // TODO: Limit framerate for preview-only mode
+        SDL_Delay(15);
+    }
+    
+    panim_engine_end_preview(&pnm);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: vid_test <OutFile>\n");
+    PAnimScene scene = {0};
+    scene.length_in_frames = 240;
+    scene.screen_width  = 1280;
+    scene.screen_height =  720;
+    
+    if (argc == 1) {
+        panim_scene_play(&scene);
+        return 0;
+    } else if (argc != 2) {
+        printf("Usage: panim <OutFile>\n");
         return 0;
     }
     
     char * filename = argv[1];
-    video_encode_test(filename);
+    panim_scene_render(&scene, filename);
     
     return 0;
 }
